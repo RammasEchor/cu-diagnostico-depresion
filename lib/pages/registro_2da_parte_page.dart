0eac0e8f7ce96da18116663dfe071bbcb012081a
RammasEchor <luis.nieto.palacios1996@gmail.com>
2021-01-07 15:24:21 -0600

    Comentarios

--- lib/pages/registro_2da_parte_page.dart
+++ lib/pages/registro_2da_parte_page.dart
@@ -5,6 +5,8 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 
 import 'package:diagnostico_depresion/clases/campo_texto.dart';
 
+// Esta clase es la segunda pantalla del registro;
+// Recibe el correo del usuario y la contraseña.
 class Registro2daPage extends StatefulWidget{
 
   @override
@@ -12,13 +14,17 @@ class Registro2daPage extends StatefulWidget{
     return Registro2daPageState();
   }
 }
+
 class Registro2daPageState extends State<Registro2daPage>{
   String _correo;
   String _password;
   String _idUsuario;
   bool _correoInvalido;
 
+  // Utilizamos un formKey para validar los campos.
   final formKey = GlobalKey<FormState>();
+  // Referencia a la base de datos; podemos manipularla con
+  // esta API.
   final databaseReference = FirebaseFirestore.instance;
 
   @override
@@ -40,14 +46,18 @@ class Registro2daPageState extends State<Registro2daPage>{
                       encabezado: 'Correo electrónico',
                       oculto: false,
                       icono: Icon(Icons.mail_rounded),
+                      // Guardamos cuando formKey invoque a save().
                       onSaved: (input) => _correo = input,
+                      // Validamos cuando formKey invoque a validate().
                       validator: _validaCorreo,
                     ),
                     CampoTexto(
                       encabezado: 'Contraseña',
                       oculto: true,
                       icono: Icon(Icons.lock_rounded),
+                      // formKey.save()
                       onSaved: (input) => _password = input,
+                      // formKey.validate()
                       validator: (input) => input.isEmpty ? '*Campo obligatorio' : null,
                     ),
                     SizedBox(height: 200,),
@@ -80,15 +90,22 @@ class Registro2daPageState extends State<Registro2daPage>{
     );
   }
 
+// Cuando presionamos el botón verde...
   void _btnListo(BuildContext context, Map parametros){
+    // Validamos.
     if(formKey.currentState.validate()){
+      // Guardamos en las variables.
       formKey.currentState.save();
 
+      // Guardamos en la base de datos.
       _guardaUsuario(parametros);
 
+      // Leemos el usuario de la base de datos.
       _obtenUsuario(_correo).then((value){
+        // Si se ingresó correctamente.
         if(value.isNotEmpty){
           _idUsuario = value[0].id.toString();
+          // Enviamos los parametros a la pantalla principal.
           Navigator.pushNamedAndRemoveUntil(context, '/inicio', (route) => false, arguments: {
             'idUsuario': _idUsuario, 'correo': _correo
           });
@@ -97,6 +114,8 @@ class Registro2daPageState extends State<Registro2daPage>{
     }
   }
 
+  // Le pedimos a la base de datos información del usuario
+  // que tenga este correo.
   Future _obtenUsuario(String correo) async{
     QuerySnapshot snapshot = await databaseReference.collection('usuarios')
         .where('correo', isEqualTo: correo).get();
@@ -105,6 +124,7 @@ class Registro2daPageState extends State<Registro2daPage>{
     return documentos;
   }
 
+  // Guarda un usuario en la base de datos.
   void _guardaUsuario(Map parametros) async {
     String sexo;
 
@@ -115,6 +135,7 @@ class Registro2daPageState extends State<Registro2daPage>{
     else
       sexo = 'otro';
 
+    // Esperamos a que lo agrege.
     DocumentReference ref = await databaseReference.collection('usuarios')
         .add({
       'correo': _correo,
@@ -125,6 +146,8 @@ class Registro2daPageState extends State<Registro2daPage>{
     });
   }
 
+  // Simplemente checamos con Regex si el correo es de la forma
+  // ejemplo@ejemplo.ejemplo
   String _validaCorreo(String valor){
     Pattern pattern =
     r'^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$';
@@ -133,6 +156,7 @@ class Registro2daPageState extends State<Registro2daPage>{
     if (!regex.hasMatch(valor))
       return '*Ingresa un correo valido';
     else {
+      // Checamos que no exista ningún usuario con ese correo.
       _obtenUsuario(valor).then((value){
         if(value.isNotEmpty)
           _correoInvalido = true;
